02677c9d7ae2253f757c1ce2e49488ba933666ac
Brady Bolton <bradybw@vt.edu>
2023-02-26 18:07:02 -0500

    feat: generate datapoints, date handling, revamped test flow

--- src/models/calculations.test.ts
+++ src/models/calculations.test.ts
@@ -1,4 +1,4 @@
-import { futureValue, futureValueSeries, pmtMonthlyToDaily, calculateCoastFire } from './calculations';
+import { futureValue, futureValueSeries, pmtMonthlyToDaily, calculateCoastFire, getDatesFormatted, generateDataSet } from './calculations';
 
 const epsilon = 0.01
 
@@ -50,3 +50,45 @@ it('calculate coast fire date unsuccessfully', () => {
     expect(result.coastFireAge).toBeUndefined();
     expect(result.finalAmount).toBeUndefined();
 });
+
+it('calculate record of 8 consecutive days', () => {
+    const today = new Date()
+    const a = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    const b = new Date(a.getFullYear(), a.getMonth(), a.getDate() + 7);
+
+    const expected = {
+        '2023-02-26': new Date(a.getFullYear(), a.getMonth(), a.getDate()),
+        '2023-02-27': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 1),
+        '2023-02-28': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 2),
+        '2023-03-01': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 3),
+        '2023-03-02': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 4),
+        '2023-03-03': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 5),
+        '2023-03-04': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 6),
+        '2023-03-05': new Date(a.getFullYear(), a.getMonth(), a.getDate() + 7),
+    }
+
+    const result = getDatesFormatted(a, b, 1)
+    expect(result).toEqual(expected);
+})
+
+it('calculate datapoints of accumulation phase', () => {
+    const data = generateDataSet(2000000, 23, 50, 0.07, 3000)
+    const expected = {
+        data: [
+            { x: '2023-02-26', y: 0 },
+            { x: '2024-06-01', y: 47534.254094779266 },
+            { x: '2025-09-05', y: 99461.99074134445 },
+            { x: '2026-12-10', y: 156189.28949854075 },
+            { x: '2028-03-15', y: 218159.7629314719 },
+            { x: '2029-06-19', y: 285858.0257015538 },
+            { x: '2030-09-23', y: 359813.48429664073 },
+            { x: '2031-12-28', y: 440604.47703725065 },
+            { x: '2033-04-02', y: 528862.796734114 },
+            { x: '2034-07-07', y: 625278.6313646277 },
+            { x: '2035-10-11', y: 730605.9614047556 }
+        ]
+    }
+
+    expect(data).toEqual(expected)
+})
+
--- src/models/calculations.ts
+++ src/models/calculations.ts
@@ -1,4 +1,7 @@
 // simple compound interest formula
+
+import { start } from "repl"
+
 // reference: https://www.thecalculatorsite.com/finance/calculators/compound-interest-formula
 const futureValue = (p: number, r: number, n: number, t: number): number => {
     return p * Math.pow(1 + (r / n), n * t)
@@ -89,4 +92,97 @@ const calculateCoastFire = (fireNumber: number, currentAge: number, retirementAg
     }
 }
 
-export { futureValue, futureValueSeries, pmtMonthlyToDaily, calculateCoastFire }
+interface CoastFireDatum {
+    x: string, // '2016-12-25'
+    y: number  // value
+}
+
+interface CoastFireData {
+    data: CoastFireDatum[]
+}
+
+const getDates = (startDate: Date, stopDate: Date, stepDays: number) => {
+    var dateArray: Date[] = new Array();
+    var currentDate = new Date(startDate);
+    while (currentDate <= stopDate) {
+        dateArray.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + stepDays);
+    }
+    return dateArray;
+}
+
+const formatDate = (date: Date): string => {
+    return date.getUTCFullYear() + "-" +
+        ("0" + (date.getUTCMonth() + 1)).slice(-2) + "-" +
+        ("0" + date.getUTCDate()).slice(-2)
+}
+
+const getDatesFormatted = (startDate: Date, stopDate: Date, stepDays: number): Record<string, Date> => {
+    let dates = getDates(startDate, stopDate, stepDays)
+
+    var result = dates.reduce(function (map: Record<string, Date>, obj: Date) {
+        const formattedDate: string = formatDate(obj)
+        map[formattedDate] = obj
+        return map;
+    }, {});
+
+    return result;
+}
+
+// shamelessly stolen: https://stackoverflow.com/a/15289883
+// we simply care about the days between, so UTC is appropriate since it does not observe
+// daylight savings, and for simplicity we will simply assume that a year is 365 days
+function dateDiffInDays(a: Date, b: Date) {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+/*
+Return the value for the outer data field used to paint the line chart
+  ...
+  data: {
+    datasets: [{
+      data: [{x: '2016-12-25', y: 1000}, {x: '2016-12-26', y: 1050}]
+    }]
+  }
+  ...
+*/
+const generateDataSet = (fireNumber: number, currentAge: number, retirementAge: number, rate: number, monthlyContribution: number): CoastFireData => {
+    const result = calculateCoastFire(fireNumber, currentAge, retirementAge, rate, monthlyContribution)
+    let data: CoastFireData = {
+        data: []
+    }
+    if (result.isPossible) {
+        // be careful: only use days for date calculations when using calculation results
+        const today = new Date()
+
+        // get chart maximum date as Date object (multiplied by 1.1 for a padding past retirement age)
+        const daysTotal = ((retirementAge - currentAge) * 365) * 1.1
+        const maxDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + daysTotal);
+
+        // get coast fire date as Date object
+        const daysAccumulating = ((result.coastFireAge ?? 0) - currentAge) * 365
+        const coastFireDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + daysAccumulating);
+
+        const coastStepSize = Math.floor(dateDiffInDays(today, coastFireDate) / 10) // TODO: figure out a better way to handle step
+        const coastFireDates: Record<string, Date> = getDatesFormatted(today, coastFireDate, coastStepSize)
+
+        for (const [dateStr, date] of Object.entries(coastFireDates)) {
+            // years elapsed since current age
+            const yearsElapsed = dateDiffInDays(today, date) / 365.0
+            const dataPoint = {
+                x: dateStr,
+                y: futureValueSeries(pmtMonthlyToDaily(monthlyContribution), rate, 365, yearsElapsed)
+            }
+            data.data.push(dataPoint)
+        }
+    }
+    return data
+
+}
+
+export { futureValue, futureValueSeries, pmtMonthlyToDaily, calculateCoastFire, getDatesFormatted, generateDataSet }
