01a0b76f18d19be0f4e12fa5b5668fb0f0378261
Brady Bolton <bradybw@vt.edu>
2023-03-02 11:43:17 -0500

    fix: finally got the formatted number input tied up

--- src/components/range/Range.tsx
+++ src/components/range/Range.tsx
@@ -1,5 +1,4 @@
-import { Input, Grid, Typography, Slider, Box, TextField } from '@mui/material'
-import InputAdornment from '@mui/material/InputAdornment';
+import { Input, Grid, Typography, Slider, Box } from '@mui/material'
 import { NumericFormat } from 'react-number-format'
 
 import React, { useState } from 'react'
@@ -16,36 +15,15 @@ interface IRangeProps {
     setState: React.Dispatch<React.SetStateAction<number>>
 }
 
-interface InputWrapperProps {
-    children?: React.ReactNode
-}
-const NumberFormatCustom: React.FC = (props: React.PropsWithRef<InputWrapperProps>) => {
-    const { ...other } = props
-
-    return (
-        <NumericFormat
-            {...other}
-            thousandSeparator
-        />
-
-    )
-}
-
 function Range(props: IRangeProps) {
 
     const [currentValue, setCurrentValue] = useState<number | number[]>(
         props.defaultValue,
     );
 
-    const [value, setValue] = useState<number>(0);
-
     const handleSliderChange = (event: Event, value: number | number[]) => {
         setCurrentValue(value);
         // props.setState(value as number) // TODO: uncomment this
-    };
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentValue(parseFloat(event.target.value));
     };
 
     const handleBlur = () => {
@@ -57,9 +35,9 @@ function Range(props: IRangeProps) {
     };
 
     // TODO: do something else about this gross sizing (trying to make mobile look good)
-    let textInputSize = 3
+    let textInputSize = 2
     if (Math.floor(Math.log10(props.maxValue)) > 2) {
-        textInputSize = 4
+        textInputSize = 3
     }
 
     let startAdornment = ""
@@ -69,7 +47,7 @@ function Range(props: IRangeProps) {
     }
     if (props.format && props.format === "percentage") {
         endAdornment = "%"
-        textInputSize = 4
+        textInputSize = 2
     }
 
     return (
@@ -98,24 +76,12 @@ function Range(props: IRangeProps) {
                         thousandSeparator=","
                         customInput={Input}
                         decimalScale={2}
-
-                    />
-                    {/*
-                    <Input
-                        value={currentValue}
-                        size="small"
-                        onChange={handleInputChange}
-                        onBlur={handleBlur}
-                        startAdornment={<InputAdornment position="start">{startAdornment}</InputAdornment>}
-                        endAdornment={<InputAdornment position="end">{endAdornment}</InputAdornment>}
-                        inputProps={{
-                            step: props.step,
-                            min: props.minValue,
-                            max: props.maxValue,
-                            type: 'number',
+                        onValueChange={(values) => {
+                            setCurrentValue(values.floatValue ?? 0)
                         }}
+                        onBlur={handleBlur}
+                        prefix={'$'}
                     />
-                    */}
                 </Grid>
             </Grid>
         </Box>
